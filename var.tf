4edf4b8ea0c31ce158f89d8b871f23071a44210c
Amar <cberg2386@gmail.com>
2024-10-08 13:17:16 +0530

    expense-infra-sg-module

---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+    
+}
+
+variable "project_name" {
+    
+}
+
+variable "environment_name" {
+    
+}
+
+variable "sg_name"{
+
+}
+
+variable "common_tags" {
+    
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
